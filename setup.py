4fe9c90d6ac954c95175b95c8486b7891597443c
王志文 <w_angzhiwen@162.com>
2019-06-25 18:14:30 +0800

    修改steup的描述

--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
 from setuptools import setup
 
+with open('README.md', 'rb') as f:
+    long_description = f.read().decode('utf-8')
+
 setup(
     name='flask_cc_wechat',
-    version='0.1',
+    version='0.1.1',
     description="Flask wechat expansion development",
-    long_description=__doc__,
+    long_description=long_description,
     license='BSD',
     author='wen',
     author_email='w_angzhiwen@163.com',
@@ -20,8 +23,11 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
-        'Topic :: Software Development :: Libraries :: Python Modules'
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Utilities',
     ]
 )
